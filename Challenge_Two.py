7f56ec5d8205ca55ec9c4504656e156cb09299ad
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-20 00:17:21 +0300

    Wrote a function positive_Values() that takes in three arguments, x,y and z and checks if exactly two values are positives

---
+++ Challenge_Two.py
@@ -0,0 +1,27 @@
+#define a function that checks if exactly two out of the three values given is positive
+def positive_Values(x, y, z):
+    #initializes a counter for the values
+    positive_count = 0
+
+#checks if the value and increment the counter is it is positive
+    if x > 0:
+        positive_count += 1
+    if y > 0:
+        positive_count += 1
+    if z > 0:
+        positive_count += 1
+
+#returns True is exactly two numbers are positive otherwise returns False
+    if positive_count == 2 and (x == 0 or y == 0 or z == 0):
+        return True
+    else:
+        return False
+
+#read the three numbers from the user
+x = int(input("Enter your first number: "))
+y = int(input("Enter your second number: "))
+z = int(input("Enter your third number: "))
+
+#calls the function positive_Values to check if exactly two numbers are positive and prints the results
+result = positive_Values(x,y,z)
+print("Results:", result)
